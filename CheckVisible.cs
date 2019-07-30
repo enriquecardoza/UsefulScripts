55febda2a9bb517de6acb98359d5eb91b6b3f163
enriquecardoza <46405970+enriquecardoza@users.noreply.github.com>
2019-07-30 20:58:37 +0200

    Add files via upload
    Visible & Scale

---
+++ CheckVisible.cs
@@ -0,0 +1,60 @@
+﻿
+using UnityEngine;
+
+public class CheckVisible : MonoBehaviour
+{
+
+	/// <summary>
+	/// You must consider that the scene camera count as a camera rendering the gameobjects
+	/// This script its usefull for example if you want to check  if diferent points are in the camera before start the script of a gameobject
+	/// </summary>
+	[SerializeField][Tooltip("All script of the gameobject")] MonoBehaviour[] scriptList;
+    [SerializeField] [Tooltip("List of gameobject to check if theirs render is visible")] GameObject[] objectsCheckRenderer;
+	ScaleElements sc;
+	private void Awake()
+	{
+		if (scriptList.Length == 0)
+			scriptList = this.GetComponentsInChildren<MonoBehaviour>();
+		if (objectsCheckRenderer.Length == 0)
+		{
+			objectsCheckRenderer = new GameObject[1];
+			objectsCheckRenderer[0] = this.gameObject;
+		}
+		foreach (MonoBehaviour mb in scriptList)
+		{
+			mb.enabled = false;
+		}
+		this.enabled = true;
+	}
+	void Update()
+    {
+
+		bool isVisible = false;
+
+		foreach(GameObject go in objectsCheckRenderer)
+		{
+			if (go.GetComponent<Renderer>().isVisible)
+				isVisible = true;
+		}
+
+
+        if (isVisible)
+        {
+            foreach (MonoBehaviour mb in scriptList)
+            {
+
+                mb.enabled = true;
+            }
+
+        }
+        else
+        {
+            foreach (MonoBehaviour mb in scriptList)
+            {
+                mb.enabled = false;
+            }
+            this.enabled = true;
+        }
+    }
+	
+}
---
+++ ScaleElements.cs
@@ -0,0 +1,97 @@
+﻿
+using UnityEngine;
+
+public class ScaleElements : MonoBehaviour
+{
+
+	[Header("Objects")]
+	[SerializeField] GameObject objectToScale;
+	[SerializeField] GameObject objectToCheck;
+	[SerializeField] GameObject cameraToCheck;
+	[Header("InitialData")]
+	[SerializeField] [Tooltip("Initial position of the gameobject")] public Vector3 posIni;
+	[SerializeField] [Tooltip("Initial scale of the gameobject")] public Vector3 scaleIni;
+	[SerializeField] float distanceIni;
+	[Header("ShowInfo")]
+	[SerializeField] [Tooltip("Distance between the came and the gameobject")] float distance;
+
+
+
+	private void Start()
+	{
+
+		if (objectToScale == null)
+		{
+			objectToScale = this.gameObject;
+		}
+		if (cameraToCheck == null)
+		{
+			cameraToCheck = UnityEngine.Camera.main.gameObject;
+		}
+		if (objectToCheck == null)
+		{
+			objectToCheck = this.gameObject;
+		}
+		scaleIni = objectToScale.transform.localScale;
+		posIni = objectToScale.transform.position;
+
+
+		Vector3 v1 = new Vector3(cameraToCheck.transform.position.x, 0, cameraToCheck.transform.position.z);
+		Vector3 v2 = new Vector3(objectToCheck.transform.position.x, 0, objectToCheck.transform.position.z);
+		distanceIni = Vector3.Distance(v2, v1);
+	}
+
+	private void Update()
+	{
+		Vector3 v1 = new Vector3(cameraToCheck.transform.position.x, 0, cameraToCheck.transform.position.z);
+		Vector3 v2 = new Vector3(objectToCheck.transform.position.x, 0, objectToCheck.transform.position.z);
+		distance = Vector3.Distance(v2, v1);
+		objectToScale.transform.localScale = Vector3.LerpUnclamped(Vector3.zero, scaleIni, distance / distanceIni);
+	}
+	public GameObject CameraToCheck
+	{
+		get
+		{
+			if (cameraToCheck == null)
+			{
+				cameraToCheck = Camera.main.gameObject;
+			}
+			return cameraToCheck;
+		}
+		set
+		{
+			cameraToCheck = value;
+		}
+	}
+	public GameObject ObjectToScale
+	{
+		get
+		{
+			if (objectToScale == null)
+			{
+				objectToScale = this.gameObject;
+			}
+			return cameraToCheck;
+		}
+		set
+		{
+			objectToScale = value;
+		}
+	}
+	public GameObject ObjectToCheck
+	{
+		get
+		{
+			if (objectToCheck == null)
+			{
+				objectToCheck = this.gameObject;
+			}
+			return objectToCheck;
+		}
+		set
+		{
+			objectToCheck = value;
+		}
+	}
+}
+
